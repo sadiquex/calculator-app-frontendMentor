87a6e4dd1ef81cfb3e73638ed7a206b3a4d89a52
sadiquex <abubakasaddik1@gmail.com>
2023-12-18 11:04:05 +0000

    added theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,30 @@ import useTheme from "./components/hooks/useTheme";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 
 export default function App() {
-  const themes = ["theme1", "theme2", "theme3"];
-  const [currentTheme, setCurrentTheme] = useState(themes[0]);
+  const [currentTheme, setCurrentTheme] = useState("theme1");
 
   const { getThemeClasses } = useTheme();
   const themeClasses = getThemeClasses(currentTheme);
+
+  const handleThemeChange = (newTheme) => {
+    setCurrentTheme(newTheme);
+  };
 
   return (
     <div
       className={`h-[100vh] flex flex-col items-center md:justify-center ${themeClasses.mainBackground}`}
     >
-      <div>
-        {themes.map((theme, i) => (
-          <button
-            key={i}
-            className={`m-4 ${
-              currentTheme === theme ? "bg-green-300" : "bg-yellow-300"
-            }`}
-            onClick={() => {
-              setCurrentTheme(themes[i]);
-            }}
-          >
-            theme {i + 1}
-          </button>
-        ))}
+      <div className="flex justify-between items-center sm:w-[330px] md:w-[400px]">
+        <span className={`text-[40px] ${themeClasses.inputTextColor}`}>
+          calc
+        </span>
+        <ThemeSwitcher
+          onThemeChange={handleThemeChange}
+          currentTheme={currentTheme}
+          themeClasses={themeClasses}
+        />
       </div>
+
       <Calculator theme={themeClasses} />
     </div>
   );
--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
-import useTheme from "./hooks/useTheme";
+const ThemeSwitcher = ({ onThemeChange, currentTheme, themeClasses }) => {
+  const themes = ["theme1", "theme2", "theme3"];
 
-const ThemeSwitcher = () => {
-  //   return { themeClasses };
+  return (
+    <div className={`rounded-full ${themeClasses.toggleBackground}`}>
+      {themes.map((theme, i) => (
+        <button
+          key={i}
+          className={`m-2 w-[20px] h-[20px] rounded-full ${
+            themeClasses.keyTextColor
+          } ${currentTheme === theme ? themeClasses.equalsKeyBackground : ""}`}
+          onClick={() => {
+            onThemeChange(themes[i]);
+          }}
+        >
+          {i + 1}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default ThemeSwitcher;
